b8f5641ab0983224b768598bd909f24460257538
bra1nslayer <ce_abdus@hotmail.com>
2014-08-23 02:02:31 +0300

    Wika
    Wifi-Turk Internet enabled AC switch for Linksys WRT54GL

---
+++ on.sh
@@ -0,0 +1,33 @@
+#!/bin/sh
+echo ""
+echo "<HTML><HEAD><TITLE>W.i.k.a by Abdu$</TITLE></HEAD>"
+echo "<BODY TEXT="green" BGCOLOR="black" ALINK="#CC0000" LINK="#CC0000" VLINK="#CC0000">"
+echo "<pre>"
+echo "+--------------------------------------------+"
+echo "|                                            |"
+echo "| __      __     .__      __                 |"
+echo "|/  \    /  \    |__|    |  | __     _____   |"
+echo "|\   \/\/   /    |  |    |  |/ /     \__  \  |"
+echo "| \        /     |  |    |    <       / __ \_|"
+echo "|  \__/\  /   /\ |__| /\ |__|_ \  /\ (____  /|"
+echo "|       \/    \/      \/      \/  \/      \/ |"
+echo "|<font size="2" face="Monaco">                          Wifi-Turk Internet Kontrollu Anahtar </font>         |"
+echo "+--------------------------------------------+"
+echo "|"
+echo "|"
+echo "|"
+echo "|"
+echo "|------[x] <font size="1" face="Monaco"><a href="on.sh">Anathari AC</a></font>"
+echo "|------[ ] <font size="1" face="Monaco"><a href="off.sh">Anahtari KAPA</a></font>"
+echo "|"
+echo "|"
+echo "+---- <font size="1" face="monaco" color="red">Abdu$ tarafindan kodlanmistir</font>"
+echo ""
+echo ""
+echo "</pre>"
+## gpio pinini acar
+gpio disable 7
+
+                      
+echo "</BODY></HTML> "
+                      
